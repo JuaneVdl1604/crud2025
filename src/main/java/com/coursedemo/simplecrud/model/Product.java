378d300665923d4c46c5e76370233c01c7a94254
juane <juanevilladeleon@gmail.com>
2025-01-06 18:59:52 -0600

    Add Product Model

---
+++ src/main/java/com/coursedemo/simplecrud/model/Product.java
@@ -0,0 +1,16 @@
+package com.coursedemo.simplecrud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+}
