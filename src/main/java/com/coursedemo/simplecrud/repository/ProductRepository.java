d768184955921dbf2a9acdf39a2ae63a37386dd9
juane <juanevilladeleon@gmail.com>
2025-01-07 18:08:03 -0600

    Update logic for findById and updateProduct

--- src/main/java/com/coursedemo/simplecrud/repository/ProductRepository.java
+++ src/main/java/com/coursedemo/simplecrud/repository/ProductRepository.java
@@ -9,31 +9,41 @@ import java.util.List;
 
 @Repository
 @Slf4j
-
 public class ProductRepository {
 
     public List<Product> products = new ArrayList<>();
 
     public List<Product> findAll(){ return products; }
 
-    public Product findById(int id){ return products.get(id); }
+    public Product findById(int id){
+
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst().orElseThrow();
+    }
     public Product save(Product product){
         products.add(product);
         return product;
     }
 
-    public Product update(Product product){
-        int index = products.indexOf(product);
+    public Product update(Product newProduct){
+        if(newProduct != null) {
+            int index = products.stream().filter(
+                    product -> product.equals(newProduct)
+            ).findFirst().hashCode();
 
-        Product updatedProduct = new Product();
-        updatedProduct.setId(product.getId());
-        updatedProduct.setName(product.getName());
-        updatedProduct.setPrice(product.getPrice());
-        updatedProduct.setDescription(product.getDescription());
-        updatedProduct.setStock(product.getStock());
+            Product updatedProduct = new Product();
+            updatedProduct.setId(newProduct.getId());
+            updatedProduct.setName(newProduct.getName());
+            updatedProduct.setPrice(newProduct.getPrice());
+            updatedProduct.setDescription(newProduct.getDescription());
+            updatedProduct.setStock(newProduct.getStock());
 
-        products.set(index,updatedProduct);
-        return updatedProduct;
+            products.set(index, updatedProduct);
+            return updatedProduct;
+        } else {
+            return null;
+        }
     }
 
     public void deleteById(int id){
