f90b73b8f3711b509b3e0f8b0711799ec895613d
juane <juanevilladeleon@gmail.com>
2025-01-08 19:04:57 -0600

    Add ProductMapper

---
+++ src/main/java/com/coursedemo/simplecrud/mappers/ProductMappers.java
@@ -0,0 +1,16 @@
+package com.coursedemo.simplecrud.mappers;
+
+import com.coursedemo.simplecrud.model.product.Product;
+import com.coursedemo.simplecrud.model.rdmProduct.RdmProduct;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface ProductMappers {
+    ProductMappers INSTANCE = Mappers.getMapper(ProductMappers.class);
+
+    @Mapping(source = "title",target = "name");
+    Product rdmProductToProduct(RdmProduct product);
+}
